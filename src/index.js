a89c6f270329fe117768a7371570e94b443b0b61
raidentek <33987212+raidentek@users.noreply.github.com>
2020-05-04 15:06:00 -0500

    gol

---
+++ src/index.js
@@ -0,0 +1,86 @@
+import React from "react";
+import Tabla from './components/Tabla'
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+import "./styles.css";
+
+function App() {
+  const [darkMode, setDarkMode] = React.useState(getInitialMode());
+  React.useEffect(() => {
+    localStorage.setItem("dark", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  function getInitialMode() {
+    const isReturningUser = "dark" in localStorage;
+    const savedMode = JSON.parse(localStorage.getItem("dark"));
+    const userPrefersDark = getPrefColorScheme();
+    // if mode was saved --> dark / light
+    if (isReturningUser) {
+      return savedMode;
+      // if preferred color scheme is dark --> dark
+    } else if (userPrefersDark) {
+      return true;
+      // otherwise --> light
+    } else {
+      return false;
+    }
+    // return savedMode || false;
+  }
+
+  function getPrefColorScheme() {
+    if (!window.matchMedia) return;
+
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  }
+
+  return (
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <nav>
+        <div className="toggle-container">
+          <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
+          <span className="toggle">
+            <input
+              checked={darkMode}
+              onChange={() => setDarkMode(prevMode => !prevMode)}
+              id="checkbox"
+              className="checkbox"
+              type="checkbox"
+            />
+            <label htmlFor="checkbox" />
+          </span>
+          <span style={{ color: darkMode ? "slateblue" : "grey" }}>☾</span>
+          {/* <button onClick={() => setDarkMode(prevMode => !prevMode)}>
+          Toggle
+        </button> */}
+        </div>
+      </nav>
+      <main>
+        <h1>{darkMode ? "Dark Mode" : "Light Mode"}</h1>
+        <h2>Toggle the switch to see some magic happen!</h2>
+
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1"/>
+          </div>
+          <div className="form-group form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+            <Tabla/>
+        </form>
+
+      </main>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
